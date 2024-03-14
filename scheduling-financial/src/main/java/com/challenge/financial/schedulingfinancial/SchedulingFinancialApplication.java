1a07db711bfeb05f2ada9c471f8ee74ef298492d
Carlos Henrique <ichfs9108@gmail.com>
2024-03-14 15:26:14 -0300

    Project Created

---
+++ scheduling-financial/src/main/java/com/challenge/financial/schedulingfinancial/SchedulingFinancialApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.financial.schedulingfinancial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingFinancialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingFinancialApplication.class, args);
+	}
+
+}
---
+++ scheduling-financial/src/test/java/com/challenge/financial/schedulingfinancial/SchedulingFinancialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.financial.schedulingfinancial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingFinancialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
