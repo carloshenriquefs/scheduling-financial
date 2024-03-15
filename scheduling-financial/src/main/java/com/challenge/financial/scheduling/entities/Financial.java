ff33f010b438426153211001ba3d221fe62142c7
Carlos Henrique <ichfs9108@gmail.com>
2024-03-14 21:25:19 -0300

    feat: Relacionamento entre entidades - User e Financial

--- scheduling-financial/src/main/java/com/challenge/financial/scheduling/entities/Financial.java
+++ scheduling-financial/src/main/java/com/challenge/financial/scheduling/entities/Financial.java
@@ -9,19 +9,29 @@ import java.time.LocalDate;
 
 @Data
 @Entity
-@Table(name = "tb_financial")
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "tb_financial")
 public class Financial {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "source_account")
     private String sourceAccount;
+    @Column(name = "target_account")
     private String targetAccount;
+    @Column(name = "transfer_amount")
     private Double transferAmount;
     private Double rate;
+    @Column(name = "data_transfer")
     private LocalDate dataTransfer;
+    @Column(name = "appointment_date")
     private LocalDate appointmentDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
 }
---
+++ scheduling-financial/src/main/java/com/challenge/financial/scheduling/entities/User.java
@@ -0,0 +1,29 @@
+package com.challenge.financial.scheduling.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(name = "last_name")
+    private String lastName;
+    private String age;
+
+    @OneToMany(mappedBy = "user")
+    private Set<Financial> financials = new HashSet<>();
+
+}
